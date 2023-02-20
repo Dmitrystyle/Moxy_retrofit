2a46c79bce4be6a2dedc8da4146a3a11b00b668f
amateuroar <kvirinson@inbox.ru>
2023-02-20 12:01:53 +0300

    add corutine secsess

--- app/src/main/java/com/example/moxy_retrofit/ui/base/BasePresenter.kt
+++ app/src/main/java/com/example/moxy_retrofit/ui/base/BasePresenter.kt
@@ -1,7 +1,19 @@
 package com.example.moxy_retrofit.ui.base
 
 import com.example.moxy_retrofit.ui.main.MainView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import moxy.MvpPresenter
+import kotlin.coroutines.CoroutineContext
 
-open class BasePresenter: MvpPresenter<MainView>() {
+open class BasePresenter: MvpPresenter<MainView>(), CoroutineScope {
+
+    private val superviserJob = Job()
+    override val coroutineContext: CoroutineContext = superviserJob + Dispatchers.Main
+
+    fun cleanup() {
+        superviserJob.cancel()
+    }
+
 }
--- app/src/main/java/com/example/moxy_retrofit/ui/main/MainPresenter.kt
+++ app/src/main/java/com/example/moxy_retrofit/ui/main/MainPresenter.kt
@@ -2,57 +2,22 @@ package com.example.moxy_retrofit.ui.main
 import com.example.moxy_retrofit.data.repository.Repository
 import com.example.moxy_retrofit.ui.base.BasePresenter
 import com.example.moxy_retrofit.utils.Constants.Companion.ERROR_MASSAGE
-import com.example.moxy_retrofit.utils.PresentalScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainPresenter() : BasePresenter() {
-
     fun requestPostPresenter(userName: String) {
-         PresentalScope.launch(Dispatchers.Main) {
+         launch() {
             try {
-
                 val list =  Repository.getRepo(userName)
                 viewState.showRepoList(list)
             } catch (e: Exception) {
                 e.printStackTrace()
                 viewState.showError(ERROR_MASSAGE)
             }
-
-        }
-
-    }
-}
-
-
-/*
-class Presenter : PlaylistContract.Presenter, CoroutineScope {
-
-    private val job = Job()
-    override val coroutineContext: CoroutineContext = job + Dispatchers.IO
-
-    override fun long fetchSongs() {
-        // This launch uses the coroutineContext defined
-        // by the coroutine presenter.
-        launch {
-            val songs = fetchSongsFromNetwork()
-            withContext(Dispatchers.Main) {
-                view?.updateSongList(songs)
-            }
         }
     }
 
-    override fun cleanup() {
-        // By default, every coroutine initiated in this context
-        // will use the job and dispatcher specified by the
-        // coroutineContext.
-        // The coroutines are scoped to their execution environment.
-        job.cancel()
-    }
 }
-*/
 
 
 
