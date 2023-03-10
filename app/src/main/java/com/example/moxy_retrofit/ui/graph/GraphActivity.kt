ce6e543b4b73bc93ee4d54088ce4d408a8b2e1d1
amateuroar <kvirinson@inbox.ru>
2023-03-10 16:57:45 +0300

    try to get data from list

--- app/src/main/java/com/example/moxy_retrofit/ui/graph/GraphActivity.kt
+++ app/src/main/java/com/example/moxy_retrofit/ui/graph/GraphActivity.kt
@@ -13,6 +13,7 @@ import com.example.moxy_retrofit.data.model.Stars
 import com.example.moxy_retrofit.ui.base.BaseActivity
 import com.example.moxy_retrofit.ui.main.MainActivity
 import moxy.ktx.moxyPresenter
+import java.text.SimpleDateFormat
 import java.util.regex.Pattern
 
 
@@ -49,31 +50,18 @@ class GraphActivity :  BaseActivity(), GraphView {
     }
 
     override fun showGraph(result: List<Stars>) {
-         repoDataList=result
+        repoDataList=result
+        val tvView3 = findViewById<TextView>(R.id.textViewSendData3)
+        val dataFormatter = SimpleDateFormat("dd-MM-yyyy")
+        val arr = repoDataList.forEach{dataFormatter.parse(it.starred_at)}
 
-       //  Toast.makeText(this@GraphActivity, repoDataList.toString(), Toast.LENGTH_SHORT).show()
-        // stringСonversion()
-         val tvView3 = findViewById<TextView>(R.id.textViewSendData3)
+        /* for (index in repoDataList) {
+            arr.add("$index $" )  //arrayData.toString().slice(17..26).toList()
+        }*/
 
-         tvView3.text = repoDataList[0].toString().slice(17..26) +"\n"+ repoDataList[1].toString().slice(17..26)
-//массив получаем
+         tvView3.text = arr.toString()                 // repoDataList[0].toString().slice(17..26)    +"\n"+ repoDataList[1].toString().slice(17..26)
 
     }
-
-/*    private fun stringСonversion() {
-     *//*   repoDataList
-        val arr = Pattern.compile(",").split(repoDataList.toString())*//*
-        val arr = repoDataList[0]
-
-        String[] lines = textView1.getText().toString().split( "\n" /* или "\r\n" */ );
-if( lines.length > 2 ) {
-    String line = lines[2];
-    return line[line.length - 1];
-} else {
-    // а что, если строк меньше трёх?
-}
-    }*/
-
     override fun showErrorGraph(massage: Int, massageType:String) {
            Toast.makeText(this, massage , Toast.LENGTH_SHORT).show()
            Toast.makeText(this, massageType , Toast.LENGTH_SHORT).show()
@@ -81,21 +69,6 @@ if( lines.length > 2 ) {
 }
 
 /*
-import java.util.regex.Pattern
-
-fun main() {
-    val str = "A:B:C"
-    val delim = "" - разделитель по пробелу
-
-    val arr = Pattern.compile(delim).split(str)
-
-    println(arr.contentToString())    // [A, B, C]
-}
-удалить до нужного символа
-s = s.substring(s.lastIndexOf('/')+1);
-
-
-
 groupBy группирует значения по некоторому критерию. Например, мы хотим разбить список котов по возрасту.
 val cats = listOf(Cat("Barsik", 5),
         Cat("Murzik", 9), Cat("Ryzhik", 5))
